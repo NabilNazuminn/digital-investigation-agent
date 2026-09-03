fdc5399903ff9baa6c5af4742cfa1226cf209a2d
NabilNazuminn <nabilnazuminn@gmail.com>
2026-09-03 22:59:42 +0700

    commit change

--- api/index.py
+++ api/index.py
@@ -1,6 +1,11 @@
 import sys
 import os
+from fastapi import FastAPI
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+app = FastAPI()
 
 from app.main import app
+@app.get("/health")
+def health():
+    return {"status": "ok"}
